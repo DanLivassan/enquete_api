07eb09f129163641f7b650691b4940977ea72184
Danilo Santana <oliveira.xc@gmail.com>
2022-10-23 11:10:19 -0300

    feat: add email validation on sign up

--- src/helpers/http.helper.ts
+++ src/helpers/http.helper.ts
@@ -1,7 +1,20 @@
-import { MissingParamError } from '../presentation/errors/missing-param-errors'
-import { HttpResponse } from '../protocol/http'
+import { InvalidParamError } from "../presentation/errors/invalid-param-error";
+import { MissingParamError } from "../presentation/errors/missing-param-errors";
+import { HttpResponse } from "../protocol/http";
 
-export const badRequest = (paramName: string): HttpResponse => ({
-  statusCode: 400,
-  body: new MissingParamError(paramName)
-})
+export const badRequest = (
+  paramName: string,
+  typeError: "MissingParam" | "InvalidParam"
+): HttpResponse => {
+  if (typeError === "MissingParam")
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName),
+    };
+
+  if (typeError === "InvalidParam")
+    return {
+      statusCode: 400,
+      body: new InvalidParamError(paramName),
+    };
+};
