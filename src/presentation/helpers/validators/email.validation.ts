c659f9ee3d8c0f03553dfeb011678c6c81f1fc9e
Danilo Santana <oliveira.xc@gmail.com>
2023-04-30 18:03:54 -0300

    refactor: use composite pattern at signUp controller

--- src/presentation/helpers/validators/email.validation.ts
+++ src/presentation/helpers/validators/email.validation.ts
@@ -3,14 +3,14 @@ import { InvalidParamError } from '../../errors'
 import { Validation } from './validation'
 
 export class EmailValidation implements Validation {
-    constructor(private readonly fieldName: string, private readonly emailValidator: EmailValidator) { }
-    validate(data: any): Error | null {
-        if (typeof data === 'object') {
-            const isValid = this.emailValidator.isValid(data[this.fieldName])
-            if (!isValid) {
-                return new InvalidParamError(`The field ${this.fieldName} is not a valid email`)
-            }
-        }
-        return null
+  constructor (private readonly fieldName: string, private readonly emailValidator: EmailValidator) { }
+  validate (data: any): Error | null {
+    if (typeof data === 'object') {
+      const isValid = this.emailValidator.isValid(data[this.fieldName])
+      if (!isValid) {
+        return new InvalidParamError(`The field ${this.fieldName} is not a valid email`)
+      }
     }
+    return null
+  }
 }
--- src/presentation/helpers/validators/required-field.validation.ts
+++ src/presentation/helpers/validators/required-field.validation.ts
@@ -2,10 +2,9 @@ import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 
 export class RequiredFieldValidation implements Validation {
-    constructor(private readonly fieldName: string) { }
-    validate(data: any): Error | null {
-        if (typeof data !== 'object' || !Object.keys(data).includes(this.fieldName))
-            return new MissingParamError(`${this.fieldName} is required`)
-        return null
-    }
+  constructor (private readonly fieldName: string) { }
+  validate (data: any): Error | null {
+    if (typeof data !== 'object' || !Object.keys(data).includes(this.fieldName)) { return new MissingParamError(`${this.fieldName} is required`) }
+    return null
+  }
 }
